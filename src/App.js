367beb473ae5fe17e29f02138d1373a07a310e87
Tejaaswini <tejnaren07@gmail.com>
2021-05-23 10:13:06 +0530

    init(routes): add routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
-import Display from './components/Display';
-import Footer from './components/Footer';
+import { Switch, BrowserRouter, Route } from "react-router-dom";
 import Header from './components/Header'
+import Display from './components/Display';
+import Misc from './components/Misc'
+import Footer from './components/Footer';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Header />
-      <Display />
+      <Switch>
+        <Route exact path="/" component={Display} />
+        <Route exact path="/artfolio/miscellaneous" component={Misc} />
+      </Switch>
       <Footer />
     </div>
+  </BrowserRouter>
   );
 }
 
